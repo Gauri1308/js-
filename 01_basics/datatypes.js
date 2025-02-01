68bc0b7e712c2ca9d6758b1d833307692dc55dbf
Gauri1308 <125488296+Gauri1308@users.noreply.github.com>
2025-02-01 07:16:47 +0000

    dtypes and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+console.log(typeof "gauri");
+console.log(typeof null);
+console.log(typeof undefined);
+
+//null is a object
+//undefined is undefined 
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+console.log("gauri");
+
+const accountId=12345;
+let accountName="gauriss";
+var accountState="delhi";
+
+//u cannot change accountId as it is const
+// var is not used due to block and functional scope 
+
+console.table([accountId,accountName,accountState]);
