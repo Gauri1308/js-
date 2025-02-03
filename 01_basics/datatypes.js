1702633367ad965b85564330804e357c8300e071
Gauri1308 <125488296+Gauri1308@users.noreply.github.com>
2025-02-03 17:09:50 +0000

    moreofdatatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,8 +1,25 @@
+//primitive(stack) and non-primitive(heap)
+
+
+
 console.log(typeof "gauri");
 console.log(typeof null);
 console.log(typeof undefined);
 
 //null is a object
 //undefined is undefined 
+let myobj={
+    books: "all is good",
+    singer: "papon"
+}
+console.log(myobj);
+console.log(typeof(myobj));
+
+const myfunc=function(){
+    console.log("hello world");
+    
+}
+
+
 
 
