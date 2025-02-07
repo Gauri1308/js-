57f153e9071386329166b531049a8584132f6819
Gauri1308 <125488296+Gauri1308@users.noreply.github.com>
2025-02-07 16:26:06 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,23 @@
+const Arr=[1,2,3,4,5];
+console.log(Arr[0]);
+
+Arr.push(7);
+Arr.push(9);
+console.log(Arr);
+
+console.log(Arr.includes(9));
+console.log(Arr.indexOf(3));
+
+// slice, splice
+
+console.log("A ", Arr);
+
+const myn1 = Arr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", Arr);//do not change original array
+
+
+const myn2 = Arr.splice(1, 3)
+console.log("C ", Arr);// chnages original array
+console.log(myn2);
