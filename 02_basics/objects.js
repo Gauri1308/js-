5b80d5594fba1d0498dd26283348a8b0bfb2040d
Gauri1308 <125488296+Gauri1308@users.noreply.github.com>
2025-02-08 16:17:25 +0000

    objects

---
+++ 02_basics/objects.js
@@ -0,0 +1,15 @@
+const obj={
+    name:"gauri",
+    age:21,
+    friends:["a","d","y"]
+
+}
+console.log(obj.friends[0]);
+
+//functions 
+obj.newf=function(){
+    console.log("hello people");
+    
+}
+console.log(obj.newf());
+
