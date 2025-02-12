de35a38d18ea0166e63630f77aa1dfb0f00d617d
Gauri1308 <125488296+Gauri1308@users.noreply.github.com>
2025-02-12 07:30:01 +0000

    objects

---
+++ 02_basics/2objects.js
@@ -0,0 +1,48 @@
+// spread operator 
+//objects 
+
+const tinderUser = new Object()
+// const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+// console.log(typeof(regularUser));
+// console.log(typeof(fullname));
+// console.log(typeof(userfullname));
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2} //spread operator 
+// console.log(obj3);
+
+// Create an Object
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50
+  };
+  
+  // Destructuring
+  let {firstName, lastName} = person;
+  let {gauri,verma}=person;
+  console.log(person);
+  
+
